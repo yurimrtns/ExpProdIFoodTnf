c5ea9e629d347bc739db096a0dc9fe3f09bdb14f
yurimrtns <150160895+yurimrtns@users.noreply.github.com>
2024-01-23 10:34:22 -0300

    COMITT

--- ApiExpIFoodTNF/CamadaDeDados/obj/Debug/net6.0/CamadaDeDados.AssemblyInfo.cs
+++ ApiExpIFoodTNF/CamadaDeDados/obj/Debug/net6.0/CamadaDeDados.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CamadaDeDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdd0729b8ab84fc4d219dd215165184a98355c53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ffe2680032e102374c1969f9a6655fb054eae5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CamadaDeDados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CamadaDeDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
